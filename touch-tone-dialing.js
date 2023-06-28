151e751897b78b924999d7a170a4a0444498fc59
helloitsjoe <jboyle617@gmail.com>
2023-06-27 21:55:28 -0400

    wip touch tone

---
+++ touch-tone-dialing.js
@@ -0,0 +1,27 @@
+const wav = require('node-wav');
+const FFT = require('fft.js');
+
+module.exports = async ({ wav_url }) => {
+  console.log('wav_url', wav_url);
+  const res = await fetch(wav_url);
+  const blob = await res.blob();
+  const arrayBuf = await blob.arrayBuffer();
+
+  const result = wav.decode(arrayBuf);
+  const input = result.channelData[0];
+  const { length } = input.length;
+
+  console.log('result', length);
+
+  const f = new FFT(4096);
+  // const input = new Array(length).fill(0);
+  const out = f.createComplexArray();
+
+  // const realInput = new Array(f.size);
+  f.realTransform(out, input);
+
+  console.log('out', out);
+  console.log('input', input);
+
+  return { sequence: '1' };
+};
